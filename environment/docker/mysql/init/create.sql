e479fbd427c04e55d2e3901f5713173e09f70685
masahiro-ikeda <masahiro.ikeda@careritz.co.jp>
2019-09-30 18:19:36 +0900

    DB環境のDocker化

---
+++ environment/docker/mysql/init/create.sql
@@ -0,0 +1,33 @@
+# ユーザー
+CREATE TABLE users(
+    id          VARCHAR(25) NOT NULL,
+    password    VARCHAR(60) NOT NULL,
+    name        VARCHAR(25) NOT NULL,
+    role        VARCHAR(10) NOT NULL,
+    is_invalid  INT(1) NOT NULL,
+    created_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+# メッセージ
+CREATE TABLE messages(
+    id          VARCHAR(36) NOT NULL PRIMARY KEY,
+    type        VARCHAR(10) NOT NULL,
+    post_no     INT(5) NOT NULL,
+    reply_no    INT(5) NOT NULL,
+    user_id     VARCHAR(25) NOT NULL,
+    comment     VARCHAR(100) NOT NULL,
+    is_deleted  INT(1) NOT NULL,
+    created_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+# いいね
+CREATE TABLE goods(
+    id          VARCHAR(36) NOT NULL PRIMARY KEY,
+    message_id  VARCHAR(36) NOT NULL,
+    user_id     VARCHAR(25) NOT NULL,
+    is_deleted  INT(1) NOT NULL,
+    created_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
